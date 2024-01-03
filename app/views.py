3bb8ddbe6de4bb3ef808b19d8fd315ba2cc351de
Allukuru Giri <allukurugiri@gmail.com>
2024-01-03 15:16:56 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Giri1(request):
+    return render(request,'Giri1.html')
+
+def sweety(request):
+    return render(request,'sweety.html')
